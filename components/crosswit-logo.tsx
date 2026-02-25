1a40022c59fcc11db295a1e4d1fdc74279d6308a
hmlinarevic <hrvoje.mlinarevic@gmail.com>
2026-02-25 23:53:03 +0100

    feat: add project link

---
+++ components/crosswit-logo.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+const BRAIN_IMAGE = "/brain.png";
+
+export function CrosswitLogo({
+  rightText = "SSWIT",
+  className = "",
+  size = "default",
+}: {
+  rightText?: string;
+  className?: string;
+  size?: "default" | "small";
+}) {
+  const fontClass = "font-[family-name:var(--font-audiowide)]";
+  const isSmall = size === "small";
+  const textSizeClass = isSmall ? "text-base" : "text-xl sm:text-2xl";
+  const iconSizeClass = isSmall ? "h-4 w-4" : "h-6 w-6";
+
+  return (
+    <div
+      className={`flex select-none items-center text-foam ${textSizeClass} ${fontClass} ${className}`}
+      data-cursor-element-id="brand-logo"
+    >
+      <span className={fontClass}>CR</span>
+      <span
+        className={`ml-px mr-px inline-block shrink-0 bg-white [mask-size:contain] [mask-repeat:no-repeat] [mask-position:center] [-webkit-mask-size:contain] [-webkit-mask-repeat:no-repeat] [-webkit-mask-position:center] ${iconSizeClass}`}
+        style={{
+          maskImage: `url(${BRAIN_IMAGE})`,
+          WebkitMaskImage: `url(${BRAIN_IMAGE})`,
+        }}
+        role="img"
+        aria-label="abstract brain symbol"
+      />
+      <span className={fontClass}>{rightText}</span>
+    </div>
+  );
+}
